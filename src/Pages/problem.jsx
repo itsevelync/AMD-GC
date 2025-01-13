3ca903a088ca22db92e8fa637368cbb18f851e0b
itsevelync <echen395@gatech.edu>
2025-01-13 11:16:06 -0500

    Fix scroll behavior

--- src/Pages/problem.jsx
+++ src/Pages/problem.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./pageLayout.css";
 import AMDImage from './amd.jpg';
 
 export default function Problem() {
   document.title = "The Problem - MineMap";
+
+  const handleScrollToTop = () => {
+    window.scrollTo(0, 0);  // Scroll to the top of the page
+  };
 
   return (
     <div className="pageContainerContainer">
@@ -13,14 +17,18 @@ export default function Problem() {
         <p className="subTitle">AMD devastates rivers and streams, posing a pressing concern for aquatic life and human health.</p>
         <div className="container" data-aos="fade-up">
           <div className="container1">
-          <div id='cleanup' className="imgContainer" style={{padding: '5px' }}><img src={AMDImage}></img>
-          <p style={{fontSize: '10px', marginBottom: 0, color: '#ffffffa2'}} >&nbsp;Source: Wayne National Forest. <a style={{color: '#ffffffa2'}} href="https://creativecommons.org/publicdomain/mark/1.0/">Creative Commons</a></p></div>
+            <div id='cleanup' className="imgContainer" style={{ padding: '5px' }}>
+              <img src={AMDImage} alt="AMD" />
+              <p style={{ fontSize: '10px', marginBottom: 0, color: '#ffffffa2' }}>
+                &nbsp;Source: Wayne National Forest. <a style={{ color: '#ffffffa2' }} href="https://creativecommons.org/publicdomain/mark/1.0/">Creative Commons</a>
+              </p>
+            </div>
             <h3 className="containerTitle">What is Acid Mine Drainage?</h3>
             <p><b>Acid Mine Drainage (AMD)</b> is a critical environmental issue in West Virginia, stemming from <b>abandoned coal mines</b> where toxic byproducts such as heavy metals and acidic runoff seep into waterways.</p>
             <p>These contaminants <b>harm aquatic ecosystems, degrade water quality, and pose significant health risks</b> to nearby communities.</p>
           </div>
         </div>
-        <br></br>
+        <br />
         <div className="container">
           <div className="container1" data-aos="fade-up">
             <h3 className="containerTitle">Why is AMD Such a Pressing Concern?</h3>
@@ -33,7 +41,9 @@ export default function Problem() {
             <p>Our mission is to <b>improve AMD identification</b>, enabling stakeholders to redirect resources from detection to remediation.</p>
           </div>
         </div>
-        <a href="/AMD-GC/solution"><button className="readMore">Read About Our Solution &gt;&gt;</button></a>
+        <a href="/AMD-GC/solution" onClick={handleScrollToTop}>
+          <button className="readMore">Read About Our Solution &gt;&gt;</button>
+        </a>
       </div>
     </div>
   );
